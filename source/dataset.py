942cb2e3a4170c2612be7cd3495a0b592b1f30cc
gabriel.pontes <gabrieliprj@gmail.com>
2019-10-17 22:52:19 -0300

    now handles input and output data

--- source/dataset.py
+++ source/dataset.py
@@ -1,3 +1,5 @@
+import torch
+
 from source.tensorizer import batchify
 
 from source.utils import build_generator
@@ -10,28 +12,37 @@ class DataIterator(object):
                  tokenizer,
                  batch_size,
                  max_len,
-                 input_dim):
+                 input_dim,
+                 lookup_labels):
         self.data = data_generator
         self.tokenizer = tokenizer
         self.batch_size = batch_size
         self.max_len = max_len
         self.input_dim = input_dim
+        self.lookup_labels = lookup_labels
 
     def __iter__(self):
         sequences = []
-        for i, x in enumerate(self.data):
+        for i, data in zip(range(self.batch_size), self.data):
+            label = torch.tensor([self.lookup_labels[data[1]]], dtype=torch.float)
             if i == self.batch_size:
                 break
-            sequences.append(x)
-        yield batchify(sequences=sequences, max_len=self.max_len, tokenizer=self.tokenizer)
+            if i == 0:
+                labels = label
+            else:
+                labels = torch.cat((labels, label), 0)
+            sequences.append(data[0])
+        yield batchify(sequences=sequences, max_len=self.max_len, tokenizer=self.tokenizer), labels
 
 
 class Dataset(object):
     def __init__(self,
-                 data_generator):
+                 data_generator,
+                 tokenizer):
         self.data = data_generator
+        self.tokenizer = tokenizer
 
-    def split(self, batch_size, max_len, input_dim, tokenizer):
+    def split(self, batch_size, max_len, input_dim, lookup_labels):
         train = test = val = []
 
         for data in self.data:
@@ -42,17 +53,20 @@ class Dataset(object):
             [val.append(data) for data in data_block[int(0.9 * len(data_block)):]]
 
         return DataIterator(data_generator=build_generator(train),
-                            tokenizer=tokenizer,
+                            tokenizer=self.tokenizer,
                             batch_size=batch_size,
                             max_len=max_len,
-                            input_dim=input_dim), \
+                            input_dim=input_dim,
+                            lookup_labels=lookup_labels), \
                DataIterator(data_generator=build_generator(test),
-                            tokenizer=tokenizer,
+                            tokenizer=self.tokenizer,
                             batch_size=batch_size,
                             max_len=max_len,
-                            input_dim=input_dim), \
+                            input_dim=input_dim,
+                            lookup_labels=lookup_labels), \
                DataIterator(data_generator=build_generator(val),
-                            tokenizer=tokenizer,
+                            tokenizer=self.tokenizer,
                             batch_size=batch_size,
                             max_len=max_len,
-                            input_dim=input_dim)
+                            input_dim=input_dim,
+                            lookup_labels=lookup_labels)
